6992d3fe5b774326740273df8f05cfa3bd700b77
1181244 <1181244@isep.ipp.pt>
2023-06-06 19:55:40 +0100

    Some corrections made regarding agent architecture

--- indoorMedia/CoreAgent.py
+++ indoorMedia/CoreAgent.py
@@ -1,10 +1,7 @@
-import threading
-from queue import Queue
 import cv2
 from DecisionMakingAgent import DecisionMakingAgent
 from DisplayAgent import DisplayAgent
 from ImageProcessingAgent import ImageProcessingAgent
-from AdvertisingAgent import AdvertisingAgent
 
 class CoreAgent:
     def __init__(self, model_paths, characteristics, img_path):
@@ -12,29 +9,15 @@ class CoreAgent:
         self.characteristics = characteristics
         self.img_path = img_path
         self.imageProcessingAgent = ImageProcessingAgent(model_paths)
-        self.ads_queue = Queue()
-        self.advertisingAgents = [AdvertisingAgent(characteristic) for characteristic in self.characteristics]
         self.displayAgent = DisplayAgent()
-        self.decisionMakingAgent = DecisionMakingAgent(self.ads_queue)
+        self.decisionMakingAgent = DecisionMakingAgent(self.characteristics)
 
     def run(self):
         # Get the demographic data from the Image Processing Agent
         img = cv2.imread(self.img_path)
         demographic_data = self.imageProcessingAgent.process_image(img)
+        winning_ad = self.decisionMakingAgent.auction(demographic_data)
 
-        # Start a thread for each Advertising Agent
-        threads = []
-        for agent in self.advertisingAgents:
-            thread = threading.Thread(target=agent.propose_ad, args=(demographic_data, self.ads_queue))
-            thread.start()
-            threads.append(thread)
-
-        # Wait for all agents to finish
-        for thread in threads:
-            thread.join()
-
-        # Run the Decision-making Agent to select the winning ad
-        winning_ad = self.decisionMakingAgent.choose_ad()
         print('WINNING AD')
         print(winning_ad)
         image_test = 'test_image_ad.jpg'
--- indoorMedia/DecisionMakingAgent.py
+++ indoorMedia/DecisionMakingAgent.py
@@ -1,12 +1,33 @@
-class DecisionMakingAgent:
-    def __init__(self, queue):
-        self.queue = queue
+import threading
+from queue import Queue
 
+from indoorMedia.AdvertisingAgent import AdvertisingAgent
+
+
+class DecisionMakingAgent:
+    def __init__(self, characteristics):
+        self.characteristics = characteristics
+        self.ads_queue = Queue()
+        self.advertisingAgents = [AdvertisingAgent(characteristic) for characteristic in self.characteristics]
+
+    def auction(self, demographic_data):
+        # Start a thread for each Advertising Agent
+        threads = []
+        for agent in self.advertisingAgents:
+            thread = threading.Thread(target=agent.propose_ad, args=(demographic_data, self.ads_queue))
+            thread.start()
+            threads.append(thread)
+
+        # Wait for all agents to finish
+        for thread in threads:
+            thread.join()
+
+        return self.choose_ad()
     def choose_ad(self):
         max_bid = 0
         winning_ad = None
-        while not self.queue.empty():
-            ad, bid = self.queue.get()
+        while not self.ads_queue.empty():
+            ad, bid = self.ads_queue.get()
             print(ad)
             print(bid)
             if bid > max_bid:
