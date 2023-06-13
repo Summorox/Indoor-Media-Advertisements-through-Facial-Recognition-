df83ef0acf8eabdd37244af9587ad838238df4d8
1181244 <1181244@isep.ipp.pt>
2023-06-13 17:27:47 +0100

    MQTT skeleton for testing

--- indoorMedia/CoreAgent.py
+++ indoorMedia/CoreAgent.py
@@ -1,16 +1,49 @@
 import cv2
+import numpy as np
+
 from DecisionMakingAgent import DecisionMakingAgent
 from DisplayAgent import DisplayAgent
 from ImageProcessingAgent import ImageProcessingAgent
+import paho.mqtt.client as mqtt
 
 class CoreAgent:
-    def __init__(self, model_paths, characteristics, img_path):
+    def __init__(self, model_paths, characteristics, img_path, mqtt_broker, mqtt_port, mqtt_topic):
         self.model_paths = model_paths
         self.characteristics = characteristics
         self.img_path = img_path
         self.imageProcessingAgent = ImageProcessingAgent(model_paths)
         self.displayAgent = DisplayAgent()
         self.decisionMakingAgent = DecisionMakingAgent(self.characteristics)
+
+        #self.client = mqtt.Client()
+        #self.client.on_connect = self.on_connect
+        #self.client.on_message = self.on_message
+
+        self.mqtt_broker = mqtt_broker
+        self.mqtt_port = mqtt_port
+        self.mqtt_topic = mqtt_topic
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+        client.subscribe(self.mqtt_topic)
+
+    def on_message(self, client, userdata, msg):
+        nparr = np.fromstring(msg.payload, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        self.process_image(img)
+
+    def process_image(self, img):
+        demographic_data = self.imageProcessingAgent.process_image(img)
+        winning_ad = self.decisionMakingAgent.auction(demographic_data)
+
+        if winning_ad.split('.')[-1] in ['jpeg', 'jpg', 'png']:
+            self.displayAgent.display_image('ads/' + winning_ad)
+        elif winning_ad.split('.')[-1] in ['avi', 'mp4']:
+            self.displayAgent.display_video('ads/' + winning_ad)
+
+    def run(self):
+        self.client.connect(self.mqtt_broker, self.mqtt_port, 60)
+        self.client.loop_forever()
 
     def run(self):
         # Get the demographic data from the Image Processing Agent
--- indoorMedia/main.py
+++ indoorMedia/main.py
@@ -15,12 +15,12 @@ mqtt_broker = 'mqtt_broker'
 mqtt_port = 1883
 mqtt_topic = 'image_processing/input'
 
-#camera_agent = CameraAgent(mqtt_broker, mqtt_port, mqtt_topic)
-#camera_agent.capture_and_send_image()
-#camera_agent.close()
+camera_agent = CameraAgent(mqtt_broker, mqtt_port, mqtt_topic)
+camera_agent.capture_and_send_image()
+camera_agent.close()
 
-#img_path = 'capture.jpg'
-img_path = 'happy-friends-from-different-races-culture-laughing_166273-465.jpg'
+img_path = 'capture.jpg'
+#img_path = 'happy-friends-from-different-races-culture-laughing_166273-465.jpg'
 
 core_agent = CoreAgent(model_paths, characteristics, img_path)
 core_agent.run()
